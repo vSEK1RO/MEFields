d93a26532abd23e66e0b68e9006466f724707d57
SEK1RO <vlad@sek1.ro>
2025-01-16 16:24:13 +0300

    feat(magneticField): worker

--- src/workers/magneticField.ts
+++ src/workers/magneticField.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three'
 import { IObject, IWire } from '../model'
+import { IFieldWorker } from './types'
+import { gridPositions, ICameraBox } from '../utils/gridPositions'
 
 export function calcMagneticField(pos: THREE.Vector3, objs: IObject[]) {
   const dir = new THREE.Vector3()
@@ -22,9 +24,21 @@ export function calcMagneticField(pos: THREE.Vector3, objs: IObject[]) {
     const mm1 = m1m_v.length()
 
     const direction = new THREE.Vector3().crossVectors(m1m2_v, m0m_v).normalize()
-    const magnitude = wire.userData.amperage * ( m0m2 / mm2 + m0m1 / mm1) / m0m
+    const magnitude = wire.userData.amperage * 10 ** -7 * ( m0m2 / mm2 + m0m1 / mm1) / m0m
 
     dir.add(direction.multiplyScalar(magnitude))
   })
   return { dir, pos }
+}
+
+
+self.onmessage = (event: MessageEvent<IFieldWorker.Request>) => {
+  const objs: IObject[] = JSON.parse(event.data.objs_json)
+  const camera_box: ICameraBox = JSON.parse(event.data.camera_box_json)
+  const positions = gridPositions(camera_box)
+
+  self.postMessage({status: 'LOADING', percentage: 50 } as IFieldWorker.Response)
+  const vectors = positions.map(pos => calcMagneticField(pos, objs))
+  self.postMessage({status: 'LOADED', vectors_json: JSON.stringify(vectors) } as IFieldWorker.Response)
+  self.close()
 }
